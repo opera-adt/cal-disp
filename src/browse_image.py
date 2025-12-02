ecaa76cd629c954f431f989599efd0dab77c6691
mgovorcin <marin.govorcin@jpl.nasa.gov>
2025-12-02 11:31:49 -0800

    Update configuration templates, adding basic logging, types and placeholder for browse image
    update gitignore
    
    remove ci tests

---
+++ src/browse_image.py
@@ -0,0 +1,64 @@
+"""Module for creating browse images for the output product."""
+
+from __future__ import annotations
+
+import cmap
+import matplotlib.pyplot as plt
+import numpy as np
+from disp_cal._types import PathOrStr
+from numpy.typing import ArrayLike
+from scipy import ndimage
+
+DEFAULT_CMAP = cmap.Colormap("vik").to_mpl()
+
+
+def _resize_to_max_pixel_dim(arr: ArrayLike, max_dim_allowed=2048) -> np.ndarray:
+    """Scale shape of a given array."""
+    if max_dim_allowed < 1:
+        raise ValueError(f"{max_dim_allowed} is not a valid max image dimension")
+    input_shape = arr.shape
+    scaling_ratio = max([max_dim_allowed / xy for xy in input_shape])
+    nan_mask = np.isnan(arr)
+    arr[nan_mask] = 0
+    arr = ndimage.zoom(arr, scaling_ratio)
+    arr[ndimage.zoom(nan_mask, scaling_ratio, order=0)] = np.nan
+    return arr
+
+
+def _save_to_disk_as_color(
+    arr: ArrayLike, fname: PathOrStr, cmap: str, vmin: float, vmax: float
+) -> None:
+    """Save image array as color to file."""
+    plt.imsave(fname, arr, cmap=cmap, vmin=vmin, vmax=vmax)
+
+
+def make_browse_image_from_arr(
+    output_filename: PathOrStr,
+    arr: ArrayLike,
+    mask: ArrayLike,
+    max_dim_allowed: int = 2048,
+    cmap: str = DEFAULT_CMAP,
+    vmin: float = -0.10,
+    vmax: float = 0.10,
+) -> None:
+    """Create a PNG browse image for the output product from given array."""
+    arr[mask == 0] = np.nan
+    arr = _resize_to_max_pixel_dim(arr, max_dim_allowed)
+    _save_to_disk_as_color(arr, output_filename, cmap, vmin, vmax)
+
+
+def make_browse_image_from_nc(
+    output_filename: PathOrStr,
+    input_filename: PathOrStr,
+    max_dim_allowed: int = 2048,
+    cmap: str = DEFAULT_CMAP,
+    vmin: float = -0.10,
+    vmax: float = 0.10,
+) -> None:
+    """Create a PNG browse image for the output product from product in NetCDF file."""
+    arr = np.nanarray(input_filename)  # placeholder
+    mask = np.nanarray  # placeholder
+
+    make_browse_image_from_arr(
+        output_filename, arr, mask, max_dim_allowed, cmap, vmin, vmax
+    )
